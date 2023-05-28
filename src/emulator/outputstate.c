cf5cd1d2de13c7ccc5046ec048d3945464b28530
Arron-Stothart <arron@mercurylabs.io>
2023-05-28 19:17:48 +0100

    Fucntion to change between big and little endian.

--- src/emulator/outputstate.c
+++ src/emulator/outputstate.c
@@ -2,6 +2,19 @@
 #include <string.h>
 #include "arm.h"
 
+// Converts hexadecimals in 32bits between endians.
+int convert(int value)
+{
+ int converted = 0;
+
+// Bitshifts for bytes.
+ converted |= ((0x000000ff & value) << 24);
+ converted |= ((0x0000ff00 & value) << 8);
+ converted |= ((0x00ff0000 & value) >> 8);
+ converted |= ((0xff000000 & value) >> 24);
+
+ return converted;
+}
 
 // Outputs state of ARM processor into .out file.
 void outputstate(ARM* arm) {
@@ -29,9 +42,8 @@ void outputstate(ARM* arm) {
 
     for (int i = 0; i < MAX_MEMORY_SIZE; i++) {
 		if (arm->memory[i] > 0) {
-            //TODO fprintf memory addresses
-            //! we recieve bytes in little endian so we have to reverse int
-            // fprintf(output, "0x%.8x: 0x%.8x\n", i, arm->memory[i]);
+            // Bytes are loaded in little endian so have to convert.
+            fprintf(output, "0x%.8x: 0x%.8x\n", i, convert(arm->memory[i]));
 		}
 	}
 
