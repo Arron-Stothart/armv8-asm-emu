37c0a510917cfdd06c5f34fdc3bab92e75bffbff
Arron-Stothart <arron@mercurylabs.io>
2023-05-30 14:15:38 +0100

    Addition of instruction type enum to defs.h

--- src/emulator/defs.h
+++ src/emulator/defs.h
@@ -4,3 +4,14 @@
 
 // Memory Constants
 #define MAX_MEMORY_SIZE (1 << 21)
+
+// Enum for Instruction Type
+typedef enum {
+    DATA_PROCESSING_IMMEDIATE,
+    DATA_PROCESSING_REGISTER, 
+    SINGLE_DATA_TRANSFER,
+    LOAD_LITERAL,
+    BRANCH,
+    NOP,
+    HALT,
+} instruction_type;
