6df8b4b9ff4e1234f32b392a84cf975770ff3f6c
Arron-Stothart <arron@mercurylabs.io>
2023-05-30 13:42:35 +0100

    fixed formatting of convert(int value)

--- src/emulator/outputstate.c
+++ src/emulator/outputstate.c
@@ -3,17 +3,16 @@
 #include "arm.h"
 
 // Converts hexadecimals in 32bits between endians.
-int convert(int value)
-{
- int converted = 0;
+int convert(int value) {
+    int converted = 0;
 
-// Bitshifts for bytes.
- converted |= ((0x000000ff & value) << 24);
- converted |= ((0x0000ff00 & value) << 8);
- converted |= ((0x00ff0000 & value) >> 8);
- converted |= ((0xff000000 & value) >> 24);
+    // Bitshifts for bytes.
+    converted |= ((0x000000ff & value) << 24);
+    converted |= ((0x0000ff00 & value) << 8);
+    converted |= ((0x00ff0000 & value) >> 8);
+    converted |= ((0xff000000 & value) >> 24);
 
- return converted;
+    return converted;
 }
 
 // Outputs state of ARM processor into .out file.
