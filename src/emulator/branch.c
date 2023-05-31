8087fd2a780f2372591cf3e9bb19c90f994cba06
Arron-Stothart <arron@mercurylabs.io>
2023-05-31 16:43:39 +0100

    Addition of bitmasks for branch instructions

--- src/emulator/branch.c
+++ src/emulator/branch.c
@@ -1,1 +1,24 @@
 #include "defs.h"
+
+// Gets branch type from instruction 
+BRANCH_TYPE getBranchType(int instruction) {
+    // TODO: Implement
+    return -1;
+}
+
+// Execute branch instruction
+void executeBranch(int offset, ARM* arm, int instruction) { // Perhaps take PC rather than entire ARM
+    BRANCH_TYPE type = getBranchType(instruction);
+    switch (type) {
+        case UNCONDITIONAL:
+            unsigned int simm26 = instruction & 0x03ffffff // TODO: Test
+            break;
+        case REGISTER:
+            unsigned int xn = instruction & 0x000003e0 // TODO: Test
+            break;
+        case CONDITIONAL:
+            unsigned int simm19 = instruction & 0x00ffffe0 // TODO: Test
+            unsigned int cond = instruction & 0x0000000f // TODO: Test
+            break;
+    }
+} 
--- src/emulator/branch.h
+++ src/emulator/branch.h
@@ -1,1 +1,7 @@
 #include "defs.h"
+
+// Gets branch type from instruction 
+BRANCH_TYPE getBranchType(int instruction);
+
+// Execute branch instruction
+void executeBranch(int offset, ARM* arm, int instruction); //Perhaps take PC rather than entire ARM
