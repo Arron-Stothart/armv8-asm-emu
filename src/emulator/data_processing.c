4d84f9c7b85adcbf15ffb021f2c1a7aedfe9448d
Arron-Stothart <arron@mercurylabs.io>
2023-06-01 14:44:23 +0100

    split up instruction

--- src/emulator/data_processing.c
+++ src/emulator/data_processing.c
@@ -1,6 +1,9 @@
 #include "defs.h"
 
 // WIP
-void dataProcessingImmediate(ARM* arm) {
-
+void dataProcessingImmediate(ARM* arm, int instruction) {
+    int sf = (instruction >> 31) & 1;
+    int opc = (instruction >> 29) & 3;
+    int opi = (instruction >> 23) & 7;
+    int operand = (instruction >> 5) & 0xFFFF1;
 }
---
+++ src/emulator/data_processing.h
@@ -0,0 +1,2 @@
+#include "defs.h"
+void dataProcessingImmediate(ARM* arm, int instruction);
