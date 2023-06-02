485652e34fa3bddae5082198b4d8ecd67aebc76b
Arron-Stothart <arron@mercurylabs.io>
2023-06-02 22:46:14 +0100

    immediate data processing framework

--- src/emulator/defs.h
+++ src/emulator/defs.h
@@ -18,14 +18,13 @@
 #define BYTES_IN_64BIT 8
 #define BYTES_IN_32BIT 4
 
-
-// Instruction Constants; codes are in big endian
+// Instruction Constants
+// Codes are in big endian
 #define INSTRUCTION_SIZE 4
 #define HALT_CODE 0x0000008a
 #define NOP_CODE 0x1f2003d5
 #define SIMM9_LEN 9
 #define SIMM12_LEN 12
-#define SIMM26_LEN 26
 
 // Single Data Processing Constants
 #define SDT_LBIT 22
@@ -36,25 +35,6 @@
 #define SDT_IBIT 11
 #define SDT_SIMM9_START 12
 #define SDT_XM_START 16
-
-// Branch Constants
-#define BR_EQ 0b0000 // Equal
-#define BR_NE 0b0001 // Not equal
-#define BR_GE 0b1010 // Signed greater or equal
-#define BR_LT 0b1011 // Signed less than
-#define BR_GT 0b1100 // Signed greater than
-#define BR_LE 0b1101 // Signed less than or equal
-#define BR_AL 0b1110 // Always
-#define BR_SIMM26_START 25
-#define BR_SIMM19_START 23
-#define BR_XN_START 9
-#define BR_COND_START 3
-#define BR_COND_LEN 4
-#define BR_DET_BITS_START 31
-#define BR_DET_BITS_LEN 3
-#define BR_DET_BITS_UNCOND 0b000
-#define BR_DET_BITS_REG 0b110
-#define BR_DET_BITS_COND 0b010
 
 // Data Processing Constants
 // (I for immediate, R for register)
@@ -67,8 +47,12 @@
 #define DPI_OPI_START 23
 #define DPI_OPI_LEN 3
 #define DPI_RD_START 0
-#define DPI_OPERAND_START 5
-#define DPI_OPERARND_LEN 18
+#define DPI_SHBIT 22
+#define DPI_IMM12_START 10 // for arithmetic
+#define DPI_RN_START 5 // for arithmetic
+#define DPI_IMM16_START 5 // for logical
+#define DPI_HW_START 21 // for logical
+#define DPI_HW_SIZE 2 // for logical
 
 // Enum for Instruction Type
 typedef enum {
@@ -92,6 +76,7 @@ typedef struct {
 
 // ARM Proccesor
 // Registers are 64 bit; Memory is byte addressable (char = 1 byte).
+// Leave CIR as char since it is an index in memory.
 typedef struct {
     uint64_t registers[NUM_OF_REGISTERS];
     char memory[MAX_MEMORY_SIZE];
