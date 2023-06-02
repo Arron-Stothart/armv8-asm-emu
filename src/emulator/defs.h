bc13a7324b09a670a9530df7df18a2da8bde6dd8
Arron-Stothart <arron@mercurylabs.io>
2023-06-02 14:02:36 +0100

    w register mask

--- src/emulator/defs.h
+++ src/emulator/defs.h
@@ -6,6 +6,7 @@
 #define REGISTER_SIZE 8 // in bytes
 #define NUM_OF_REGISTERS 31
 #define ZR 0 // Zero register
+#define WREGISTER_MASK 0x0000000011111111 // sets top 32 bits to 0.
 #define REG_INDEX_SIZE 5 // number of bits used in instructions
 
 // Memory Constants
