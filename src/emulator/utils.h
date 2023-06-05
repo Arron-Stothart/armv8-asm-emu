db671fb8731313a2953b54b8d8df1edb4197dec7
Arron-Stothart <arron@mercurylabs.io>
2023-06-05 20:49:30 +0100

    change return types for asr and shiftrm array

--- src/emulator/utils.h
+++ src/emulator/utils.h
@@ -19,7 +19,7 @@ int getWord(char* memory);
 uint64_t ror(uint64_t value, int shift, bool is64bit);
 
 // Arithemtic shift right
-uint64_t asr(int64_t value, int shift, bool is64bit);
+uint64_t asr(uint64_t value, int shift, bool is64bit);
 
 // Logical shift left
 uint64_t lsl(uint64_t value, int shift, bool is64bit);
