58f0db5572f5aba603bb660f04bd39935c361255
Arron-Stothart <arron@mercurylabs.io>
2023-06-05 21:32:14 +0100

    Variables for components of instruction

--- src/emulator/data_transfer.c
+++ src/emulator/data_transfer.c
@@ -2,19 +2,18 @@
 #include "utils.h"
 
 static TRANSFER_TYPE getTransferType(int instruction) {
-    // TODO 
+    // TODO
     return -1;
 }
 
 // Execute single data transfer.
 void singleDataTransfer(ARM* arm, int instruction) {
-    // int lbit = getBitAt(instruction, SDT_LBIT);
-    // int ubit = getBitAt(instruction, SDT_UBIT);
-    // int sfbit = getBitAt(instruction, SDT_SFBIT);
-    // int ibit = getBitAt(instruction, SDT_IBIT);
-
-    // int targetreg = getBitsAt(instruction, SDT_RT_START, REG_INDEX_SIZE);
-    // int basereg = getBitsAt(instruction, SDT_XN_START, REG_INDEX_SIZE);
+    int lbit = getBitAt(instruction, SDT_LBIT_POS);
+    int ubit = getBitAt(instruction, SDT_UBIT_POS);
+    int sfbit = getBitAt(instruction, SDT_SFBIT_POS);
+    int ibit = getBitAt(instruction, SDT_IBIT_POS);
+    int rt = getBitsAt(instruction, SDT_RT_START, REG_INDEX_SIZE);
+    int xn = getBitsAt(instruction, SDT_XN_START, REG_INDEX_SIZE);
 
     TRANSFER_TYPE type = getTransferType(instruction);
 
