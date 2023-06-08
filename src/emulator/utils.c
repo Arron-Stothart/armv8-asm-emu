9b1f1b89c6ae71bc2dfe4cb5dd83111ea28702de
Arron-Stothart <arron@mercurylabs.io>
2023-06-08 16:52:18 +0100

    remove convert

--- src/emulator/utils.c
+++ src/emulator/utils.c
@@ -5,19 +5,6 @@
 #include <inttypes.h>
 #include "defs.h"
 
-// Converts values in 32bits between endians.
-int convert(int32_t value) {
-    int converted = 0;
-
-    // Bitshifts for bytes.
-    converted |= ((0x000000ff & value) << 24);
-    converted |= ((0x0000ff00 & value) << 8);
-    converted |= ((0x00ff0000 & value) >> 8);
-    converted |= ((0xff000000 & value) >> 24);
-
-    return converted;
-}
-
 // Returns word from byte addressable memory
 int getWord(char* memory) {
     int value = 0;
