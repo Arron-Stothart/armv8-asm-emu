ca30418fabaabd49ddc4a1ecd9b359223ef88343
Arron-Stothart <arron@mercurylabs.io>
2023-06-12 15:10:05 +0100

    prototype and skelleton for tokenize

--- src/assembler/tokenize.c
+++ src/assembler/tokenize.c
@@ -1,1 +1,20 @@
+#include <string.h>
 #include "tokenize.h"
+
+// Breaks a line into its label, opcode and operand field(s)
+// Header types are temporary
+void tokenize(char* line) {
+    char* saveptr;
+    char* token;
+
+    // Get first token from line
+    token = strtok_r(line, " ", &saveptr);
+    
+    // TODO: deal with this token
+
+    // Get successive tokens until end of line
+    while (token != NULL) {
+        token = strtok_r(line, " ", &saveptr);
+         // TODO: deal with this token
+    }
+}
--- src/assembler/tokenize.h
+++ src/assembler/tokenize.h
@@ -1,2 +1,6 @@
 #include "defs.h"
 #include "utils.h"
+
+// Breaks a line into its label, opcode and operand field(s)
+// Header types are temporary
+void tokenize(char* line);
