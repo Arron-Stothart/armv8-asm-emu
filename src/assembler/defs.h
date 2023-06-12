58afc9d0d586d712b82934f3a85e7cf8f6cf4f03
Arron-Stothart <arron@mercurylabs.io>
2023-06-12 15:09:09 +0100

    Enum for assembly line type (maybe not needed)

--- src/assembler/defs.h
+++ src/assembler/defs.h
@@ -1,7 +1,17 @@
 #include <stdint.h>
 
 // Entries for symbol table: associates a label with memory address
-typedef struct symbol {
+typedef struct {
     char* label;
     uint64_t address;
-} symbol_t;
+ } symbol;
+
+// Enum for type of assembly line; possibly not needed
+// INSTRUCTION = an operation mnemonic (e.g. add, ldr, . . . ), and one, two, three or four operand fields
+// DIRECTIVE = strings that start with a ‘.’; the only one is ‘.int’
+// LABEL = strings that begin with an alphabetical character (a–z or A–Z) and end with a ‘:’ 
+typedef enum {
+    INSTRUCTION, 
+    DIRECTIVE,
+    LABEL        
+} LINE_TYPE;
