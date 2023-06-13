2400a62af08dab5bb98a6b67bfaa5195f66abe66
Arron-Stothart <arron@mercurylabs.io>
2023-06-13 15:21:29 +0100

    Open input file in main

--- src/assembler/assemble.c
+++ src/assembler/assemble.c
@@ -1,5 +1,13 @@
 #include <stdlib.h>
+#include <stdio.h>
 
 int main(int argc, char **argv) {
+
+  if (argc < 3) {
+        fprintf(stderr, "emulate: no assembler file provided.\n");
+        exit(EXIT_FAILURE);
+    }
+
+  FILE *input = fopen(argv[1], "r");
   return EXIT_SUCCESS;
 }
