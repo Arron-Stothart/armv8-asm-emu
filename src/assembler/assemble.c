e2456437b7f5984269e76fbe9c450f1746008fb9
Arron-Stothart <arron@mercurylabs.io>
2023-06-14 18:00:12 +0100

    Fix indentation

--- src/assembler/assemble.c
+++ src/assembler/assemble.c
@@ -6,16 +6,16 @@
 int main(int argc, char **argv) {
 
   if (argc < 3) {
-        fprintf(stderr, "emulate: no assembler file provided.\n");
-        exit(EXIT_FAILURE);
+      fprintf(stderr, "emulate: no assembler file provided.\n");
+      exit(EXIT_FAILURE);
   }
 
   // Read assembly file and pass each line to parser
   FILE* input = fopen(argv[1], "r");
 	// If file pointer is null then return with error
 	if (input == NULL) {
-        printf("Error in opening file.\n");
-        exit(EXIT_FAILURE);
+      printf("Error in opening file.\n");
+      exit(EXIT_FAILURE);
   }
 
   // First pass: Create symbol table associating labels with memory addresses
