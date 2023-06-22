1a0852250385ec8fd5d702cefffec65768b7417c
Arron-Stothart <arron@mercurylabs.io>
2023-06-22 16:33:18 +0100

    remove assembler from master

--- src/assembler/tokenize.h
+++
@@ -1,8 +0,0 @@
-#include "defs.h"
-#include "utils.h"
-
-// Break a line into tokens label, opcode and operand field(s)
-// void tokenize(char* line);
-
-// Convert a line known to be an instruction into an instruction type
-instruction tokenizeInstruction(char* line);
