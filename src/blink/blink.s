6ba0d8c7465bb93f99723a4988bf0130ee42b4e5
Arron-Stothart <arron@mercurylabs.io>
2023-06-23 11:00:17 +0100

    add blink

---
+++ src/blink/blink.s
@@ -0,0 +1,16 @@
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+loop:
+b loop
+
+COMMENTS:
+0x3f000000 + 0xb880 = 0x3F00B880
+https://adamransom.github.io/posts/raspberry-pi-bare-metal-part-1.html
